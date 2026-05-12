2c7dc7dcce4c9756d3090cef75e6c139f53ea158
Muhammadali Saidov <saidovdevfull@gmail.com>
2026-05-12 09:49:04 +0500

    Simplify user object in login response

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -74,7 +74,7 @@ class AuthController {
       return res.status(200).json({
         success: true,
         message: "Login successful",
-        user: { id: student.id, email: student.email },
+        user: student,
         token,
       });
     } catch (error) {
